fda0a01d3da5c00a57b4524189290f0b40dd28d4
Rebekah Monson <rebekahmonson@gmail.com>
2013-11-06 13:11:22 -0500

    Rails Girls App
    Set up my first rails app, an ideas app.
    Includes a way to upload text and photos.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '2da79ae1f42b4ef7ae899f4476d353aae120429dc7000968a529c060d3f7f0ee9bee02bd9d12eec772756222baf745f09bdf059bfc74601d5d6a88af71abff63'
